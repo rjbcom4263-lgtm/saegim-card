54f34b478038822ec31d637e5f3722a3694e761c
saegim <rjbcom4263@gmail.com>
2026-07-06 11:28:23 +0900

    PDF 일괄 저장 + 배치 미리보기 완성

---
+++ deploy.bat
@@ -0,0 +1,12 @@
+@echo off
+del /f /q ".git\index.lock" 2>nul
+del /f /q ".git\HEAD.lock" 2>nul
+del /f /q ".git\REBASE_HEAD.lock" 2>nul
+del /f /q ".git\MERGE_HEAD.lock" 2>nul
+del /f /q ".git\packed-refs.lock" 2>nul
+git add -A
+git commit -m "%~1"
+git push
+firebase deploy --only hosting
+echo.
+echo Done!
